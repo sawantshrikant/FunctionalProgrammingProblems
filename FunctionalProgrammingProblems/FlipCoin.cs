4fc0c56aab4f4e2c79b988e8ba34dde449c71dfb
Shrikant <sawantshrikant07@gmail.com>
2023-03-08 15:23:52 +0530

    [Shrikant] add:FlipCoin&PrintPercentageOfHeads&Tails

---
+++ FunctionalProgrammingProblems/FlipCoin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FunctionalProgrammingProblems
+{
+    public class FlipCoin
+    {
+        const double VALIDATION_CHECK = 0.5;
+
+        public void Flip(int numOfTimes)
+        {
+            int head = 0, tail = 0;
+            Random random = new Random();
+            for (int i = 0; i < numOfTimes; i++)
+            {
+                double value = random.NextDouble();
+                if (value < VALIDATION_CHECK)
+                    tail++;
+                else
+                    head++;
+            }
+
+            double headPercentage = ((double)head / numOfTimes) * 100;
+            double tailPercentage = ((double)tail / numOfTimes) * 100;
+            Console.WriteLine("Head Percentage---> " + headPercentage + "\nTail Percentage---> " + tailPercentage);
+        }
+    }
+}
--- FunctionalProgrammingProblems/Program.cs
+++ FunctionalProgrammingProblems/Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
+﻿using FunctionalProgrammingProblems;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to FunctionalProgrammingProblems!");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Choose an option to execute\n1.Flip coin\n2.Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin flip = new FlipCoin();
+                    Console.WriteLine("Enter num of flips");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    flip.Flip(num);
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
