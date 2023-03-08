9e7b60177ca844d222b4a1779d488704d1455e6b
Shrikant <sawantshrikant07@gmail.com>
2023-03-08 15:37:18 +0530

    [Shrikant] add:LeapYear

---
+++ FunctionalProgrammingProblems/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FunctionalProgrammingProblems
+{
+    public class LeapYear
+    {
+        public void CheckLeapYear(int year)
+        {
+            if (year.ToString().Length != 4)
+            {
+                Console.WriteLine("Please enter a 4 digit year.");
+                return;
+            }
+
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine(year + " is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year.");
+            }
+        }
+    }
+}
--- FunctionalProgrammingProblems/Program.cs
+++ FunctionalProgrammingProblems/Program.cs
@@ -8,7 +8,7 @@ internal class Program
         bool flag = true;
         while (flag)
         {
-            Console.WriteLine("Choose an option to execute\n1.Flip coin\n2.Exit");
+            Console.WriteLine("Choose an option to execute\n1.Flip coin\n2.Leap year\n3.Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -19,7 +19,16 @@ internal class Program
                     flip.Flip(num);
                     break;
                 case 2:
+                    LeapYear leapYear = new LeapYear();
+                    Console.WriteLine("Enter a year:");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    leapYear.CheckLeapYear(year);
+                    break;
+                case 3:
                     flag = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please choose again.");
                     break;
             }
         }
